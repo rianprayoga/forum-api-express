515f9d2071ff9a494a8c75a664bfb85dbee33fe9
rian <junianto.riansp@gmail.com>
2025-08-27 16:16:14 +0700

    feat: enable cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,9 +2,12 @@ import express from "express"
 import { publicRouter } from "../route/public-router"
 import { errorMiddleware } from "../middleware/error-middleware"
 import { authenticatedRouter } from "../route/auth-router"
+import cors from "cors"
 
 export const web = express()
+web.use(cors())
 web.use(express.json())
+
 web.use(publicRouter)
 web.use(authenticatedRouter)
 web.use(errorMiddleware)
