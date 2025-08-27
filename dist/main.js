f5e27a786d5f8a701a58a28f33d4fb4720979508
rianprayoga <23112312+rianprayoga@users.noreply.github.com>
2025-08-27 14:00:14 +0700

    Delete dist directory

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logging_1 = require("./application/logging");
-const web_1 = require("./application/web");
-web_1.web.listen(3000, () => {
-    logging_1.logger.info("running in port 3000");
-});
